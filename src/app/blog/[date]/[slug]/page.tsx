3c133ce9f51cf600e7627eb290bcdb7551b4c8d5
Mati <matcwi98@gmail.com>
2024-02-13 17:50:12 +0100

    remove blog

--- src/app/blog/[date]/[slug]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default async function BlogPage({ params }: { params: { date: string; slug: string } }) {
-	return (
-		<div>
-			<h1>
-				Blog {params.date} {params.slug}
-			</h1>
-		</div>
-	);
-}
