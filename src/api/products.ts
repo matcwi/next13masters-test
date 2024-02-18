2ef052dd7041f4aec581fc88d7180603e90a5310
Mati <matcwi98@gmail.com>
2024-02-18 20:02:45 +0100

    feat(module-3): Add gql config

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { ProductsGetListDocument } from "@/gql/graphql";
 import { type ProductItemType } from "@/ui/types";
 
 type ProductResponseItem = {
@@ -14,12 +19,53 @@ type ProductResponseItem = {
 	longDescription: string;
 };
 
+const executeGraphql = async (query, variables) => {
+	if (!process.env.GRAPHQL_URL) {
+		throw TypeError("GQL is not defined");
+	}
+	const res = await fetch(process.env.GRAPHQL_URL, {
+		method: "POST",
+		body: JSON.stringify({ query, variables }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const gqlResponse = await res.json();
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+	if (gqlResponse.errors) {
+		throw TypeError("gql error");
+	}
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+	return gqlResponse.data;
+};
+
 export const getProductsList = async (offset = 0) => {
 	const res = await fetch(`https://naszsklep-api.vercel.app/api/products?take=20&offset=${offset}`);
 	const productsResponse = (await res.json()) as ProductResponseItem[];
 	const products = productsResponse.map(productResponeItemToItemType);
 
-	return products;
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const gqlResponse = await executeGraphql(ProductsGetListDocument, {});
+
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+	return gqlResponse.products.data.map(
+		(p: { id: any; name: any; price: any; description: any; images: { url: any }[] }) => {
+			return {
+				id: p.id,
+				category: "",
+				name: p.name,
+				price: p.price,
+				description: p.description,
+				coverImage: {
+					src: p.images[0].url,
+					alt: p.name,
+				},
+			};
+		},
+	);
 };
 
 export const getProductById = async (id: ProductResponseItem["id"]) => {
