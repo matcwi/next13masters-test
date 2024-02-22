9256757f952b19dc1fcc1fbab07e46c9543a1350
Mati <matcwi98@gmail.com>
2024-02-22 21:58:03 +0100

    Fix gql data

--- src/ui/organisms/SuggestedProducts.tsx
+++ src/ui/organisms/SuggestedProducts.tsx
@@ -7,5 +7,5 @@ export const SuggestedProductsList = async () => {
 	const products = await getProductsList();
 	await sleep(5000);
 
-	return <ProductList products={products.slice(-4)} />;
+	return <ProductList products={products.data.slice(-4)} />;
 };
